a7ab8b89748860f8d747cff50d8f38e6d25fb005
Alan Xu <alan.xu1699@gmail.com>
2022-05-20 19:08:48 -0400

    commit tree ff85d5635eadcfe3893d55833f8bb1da17ba8df3

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield",
+]
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const copyOfCats = [...cats, name]
+    return copyOfCats; 
+}
+
+function prependCat(name) {
+    const copyOfCats = [name, ...cats]
+    return copyOfCats;
+}
+
+function removeLastCat(name) {
+    return cats.slice(0,-1)
+}
+
+function removeFirstCat(name) {
+    return cats.slice(1);
+}
